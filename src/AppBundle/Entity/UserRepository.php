4b222f6596354ff400f618698ba4d91ec2563b6c
Colin Frei <colin@colinfrei.com>
2015-11-13 15:18:07 +0100

    this doesn't work - let it load with more queries

--- src/AppBundle/Entity/UserRepository.php
+++ src/AppBundle/Entity/UserRepository.php
@@ -7,8 +7,7 @@ class UserRepository extends \Doctrine\ORM\EntityRepository
     public function findUsersWithCopiesByEmail(array $emails)
     {
         $qb = $this->createQueryBuilder('u');
-        $qb->join('u.copies', 'c')
-            ->where($qb->expr()->in('u.email', $emails));
+        $qb->where($qb->expr()->in('u.email', $emails));
 
         $query = $qb->getQuery();
 
