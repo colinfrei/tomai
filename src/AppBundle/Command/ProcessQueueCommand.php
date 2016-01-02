dd0f4a575b74e34e1c4fbf9fa2eb501e73e5b071
Colin Frei <colin@colinfrei.com>
2016-01-02 23:54:22 +0100

    cleanup

--- src/AppBundle/Command/ProcessQueueCommand.php
+++ src/AppBundle/Command/ProcessQueueCommand.php
@@ -6,9 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ChoiceQuestion;
-use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Routing\Router;
 
 class ProcessQueueCommand extends ContainerAwareCommand
 {
--- src/AppBundle/FormHelp.php
+++ src/AppBundle/FormHelp.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class FormHelp extends AbstractTypeExtension
 {
